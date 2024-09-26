dbf5aae7922cbbcc946f74e1c33cd067c514f664
dbih91 <179danil@gmail.com>
2024-09-26 14:19:50 +0300

    header added

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,18 @@
 </script>
 
 <template>
+  <header>
+    <router-link
+      class="logo"
+      to="/"
+      >
+      <img
+        src="/logo.png"
+        alt="TT Spaceships logo"
+      >
+      TT Spaceships
+    </router-link>
+  </header>
   <main>
     <router-view />
   </main>
