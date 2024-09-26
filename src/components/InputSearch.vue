24f52256a08dd6c4cea09da057d35063c0491ed7
dbih91 <179danil@gmail.com>
2024-09-26 14:25:31 +0300

    InputSearch.vue was added

---
+++ src/components/InputSearch.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+interface InputSearchProps {
+  placeholder: string
+}
+
+defineModel('modelValue', { default: '' })
+
+withDefaults(defineProps<InputSearchProps>(), {
+  placeholder: 'Search'
+})
+</script>
+
+<template>
+  <label>
+    <input
+      v-model="modelValue"
+      type="search"
+      :placeholder="placeholder"
+    >
+  </label>
+</template>
+
+<style lang="scss">
+
+</style>
