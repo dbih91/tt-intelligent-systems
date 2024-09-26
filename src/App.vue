2f68b20aae3d5a2a5a1eb9ee32fdddf1a027f74e
dbih91 <179danil@gmail.com>
2024-09-26 14:48:13 +0300

    github link added to header

--- src/App.vue
+++ src/App.vue
@@ -13,6 +13,16 @@
       >
       TT Starships
     </router-link>
+    <a
+      class="git-link"
+      href="https://github.com/dbih91/tt-intelligent-systems"
+      target="_blank"
+    >
+      <img
+        src="/github-mark-white.svg"
+        alt="Github logo"
+      />
+    </a>
   </header>
   <main>
     <router-view />
