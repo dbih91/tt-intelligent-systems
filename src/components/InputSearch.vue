bd74199784ed02d09515d1551ca63c881ecef8c1
dbih91 <179danil@gmail.com>
2024-09-26 14:34:11 +0300

    input search integrated

--- src/components/InputSearch.vue
+++ src/components/InputSearch.vue
@@ -3,7 +3,7 @@ interface InputSearchProps {
   placeholder: string
 }
 
-defineModel('modelValue', { default: '' })
+const modelValue = defineModel('modelValue', { default: '' })
 
 withDefaults(defineProps<InputSearchProps>(), {
   placeholder: 'Search'
@@ -11,15 +11,48 @@ withDefaults(defineProps<InputSearchProps>(), {
 </script>
 
 <template>
-  <label>
+  <label class="tt-input-search">
     <input
+      class="tt-input-search__input"
       v-model="modelValue"
       type="search"
       :placeholder="placeholder"
     >
+    <span class="tt-input-search__detail">
+      🔍
+    </span>
   </label>
 </template>
 
 <style lang="scss">
+.tt-input-search {
+  position: relative;
 
+  &__input {
+    border-radius: 8px;
+    border: 1px solid transparent;
+    padding: 0.6rem 2.4rem 0.6rem 0.6rem;
+    font-size: 1rem;
+    background-color: #1a1a1a;
+    cursor: pointer;
+    transition: border-color 250ms;
+
+    &:hover {
+      border-color: #646cff;
+    }
+
+    &:focus,
+    &:focus-visible {
+      outline: 4px auto -webkit-focus-ring-color;
+    }
+  }
+
+  &__detail {
+    position: absolute;
+    pointer-events: none;
+    user-select: none;
+    right: 0.6rem;
+    top: -0.1rem;
+  }
+}
 </style>
--- src/pages/StarshipsPage.vue
+++ src/pages/StarshipsPage.vue
@@ -1,10 +1,16 @@
 <script setup lang="ts">
+import InputSearch from '../components/InputSearch.vue'
+import { ref } from 'vue'
 
+const search = ref<string>()
 </script>
 
 <template>
   <div>
-
+    <InputSearch
+      v-model="search"
+      placeholder="Search Starship"
+    />
     StarshipPage
   </div>
 </template>
