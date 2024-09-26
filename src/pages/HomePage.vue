4f8eda83519016510820d085560f0c181f0bb8f4
dbih91 <179danil@gmail.com>
2024-09-26 14:38:28 +0300

    Home page added

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -3,11 +3,39 @@
 </script>
 
 <template>
-  <div>
-    Welcome
+  <div class="tt-home-page">
+    <h1>
+      Starships Control Panel Application
+    </h1>
+    <p>
+      Test task
+    </p>
+    <p>
+      Author Petrov Danil
+    </p>
+    <a
+      href="https://github.com/dbih91/tt-intelligent-systems"
+      target="_black"
+    >
+      Github Page
+    </a>
+    <router-view to="/starships">
+      <button>
+        Starships
+      </button>
+    </router-view>
   </div>
 </template>
 
 <style lang="scss">
+.tt-home-page {
+  display: flex;
+  flex-flow: column;
+  gap: 0.5rem;
+  align-items: center;
 
+  h1 {
+    margin-bottom: 1rem;
+  }
+}
 </style>
